06b02e16a575d363081705bd97a58a3c01114063
Jakob <jakob.lindqvist@chalmers.se>
2020-05-03 18:03:27 +0200

    Api first try

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,8 +2,16 @@
 
 #[macro_use]
 extern crate rocket;
+use rocket::http::Method; // 1.
 use rocket::response::status::NotFound;
 use rocket_contrib::json::Json;
+use rocket_cors::{
+    AllowedHeaders,
+    AllowedOrigins,
+    Cors,
+    CorsOptions, // 3.
+    Error,       // 2.
+};
 use wwc_data::lsv;
 
 #[get("/<file_name>")]
@@ -14,9 +22,43 @@ fn index(file_name: String) -> Result<Json<lsv::Data>, NotFound<String>> {
             Err(err) => return Err(NotFound(err.to_string())),
         };
     let data: lsv::Data = serde_json::from_str(&data_json).expect("JSON format error.");
+    println!("{:?}", &data);
     Ok(Json(data))
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+fn make_cors() -> Cors {
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        // 4.
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:8000",
+        "http://0.0.0.0:8000",
+    ]);
+
+    CorsOptions {
+        // 5.
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin", // 6.
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS")
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(make_cors()) // 7.
+}
+
+fn main() -> Result<(), Error> {
+    // 2.
+    rocket().launch();
+    Ok(())
 }
